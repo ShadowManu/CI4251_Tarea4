be9627b15fe81ff865d6d3f662759473ebe2e227
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-06-26 19:46:50 -0400

    sent version

--- Bathroom.hs
+++ Bathroom.hs
@@ -1,11 +1,23 @@
-import Locks
-import Queue
+import System.Environment (getArgs)
 
-import Control.Monad (forever)
+import Queue (runClassic)
+
 ---- MAIN PROGRAM
 
 main :: IO ()
 main = do
-  queue <- newQueue
-  locks <- newLocks
-  forever $ decreaseQueue queue locks
+  args <- getArgs
+  if null args
+    then usage
+    else case head args of
+      "classic" -> runClassic
+      "transactional" -> error "Not Implemented"
+      _ -> usage
+
+usage :: IO ()
+usage = do
+  putStrLn $
+    "El programa acepta un argumento por linea de comandos\n"
+    ++ "para indicar el tipo de ejecucion:\n"
+    ++ "  ./Bathroom classic\n"
+    ++ "  ./Bathroom transactional"
